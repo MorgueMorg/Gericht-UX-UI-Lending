12773ee60025c6c123f15e905d89aad606e98dce
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2022-09-10 22:42:21 +0600

    Finished footer section :game_die:

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import './FooterOverlay.css';
+import "./FooterOverlay.css";
 
 const FooterOverlay = () => (
-  <div>
-    FooterOverlay
+  <div className="app__footerOverlay">
+    <div className="app__footerOverlay-black" />
+    <div className="app__footerOverlay-img app__bg" />
   </div>
 );
 
