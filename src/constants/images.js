3ee62c134a54d7b266cf225dd439d27e9a9aa570
sun-jay <sunny.jyrm@gmail.com>
2023-07-02 21:26:42 -0700

    images

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,18 +1,18 @@
-import bg from '../assets/bg.png';
-import chef from '../assets/chef.png';
-import G from '../assets/G.png';
 import gallery01 from '../assets/theemu_gallery01.png';
 import gallery02 from '../assets/theemu_gallery02.png';
 import gallery03 from '../assets/theemu_gallery03.png';
 import gallery04 from '../assets/theemu_gallery04.png';
 import knife from '../assets/theemu_knife.png';
-import logo from '../assets/logo.png';
+import welcome from '../assets/theemu_welcome.png';
 import menu from '../assets/theemu_menu.png';
+import laurels from '../assets/theemu_laurels.png';
+import bg from '../assets/bg.png';
+import chef from '../assets/chef.png';
+import G from '../assets/G.png';
+import logo from '../assets/logo.png';
 import overlaybg from '../assets/overlaybg.png';
 import spoon from '../assets/spoon.svg';
-import welcome from '../assets/theemu_welcome.png';
 import findus from '../assets/findus.png';
-import laurels from '../assets/theemu_laurels.png';
 import award01 from '../assets/award01.png';
 import award02 from '../assets/award02.png';
 import award03 from '../assets/award03.png';
@@ -21,7 +21,6 @@ import sign from '../assets/sign.png';
 import quote from '../assets/quote.png';
 import gericht from '../assets/gericht.png';
 
-theemu_
 
 export default {
   bg,
