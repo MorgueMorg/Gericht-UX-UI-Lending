712556a6da3c20e35d07874d9345472215a1a467
sun-jay <sunny.jyrm@gmail.com>
2023-07-04 21:10:11 -0700

    knife

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -4,7 +4,6 @@ import { images } from "../../constants";
 import "./AboutUs.css";
 import Themu from "../../components/Themu";
 
-
 const AboutUs = () => (
   <div
     className="app__aboutus app__bg flex__center section__padding"
@@ -20,9 +19,9 @@ const AboutUs = () => (
         {/* <img src={images.spoon} alt="about_spoon" className="spoon__img" /> */}
         <p className="p__opensans">
           <Themu>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra
-            adipiscing ultrices vulputate posuere tristique. In sed odio nec
-            aliquet eu proin mauris et.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis
+            pharetra adipiscing ultrices vulputate posuere tristique. In sed
+            odio nec aliquet eu proin mauris et.
           </Themu>
         </p>
         <button type="button" className="custom__button">
@@ -31,7 +30,14 @@ const AboutUs = () => (
       </div>
 
       <div className="app__aboutus-content_knife flex__center">
-        <img src={images.knife} alt="about_knife" />
+        <img
+          style={{
+            width: "auto",
+            height: "300px",
+          }}
+          src={images.knife}
+          alt="about_knife"
+        />
       </div>
 
       <div className="app__aboutus-content_history">
