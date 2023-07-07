92fd02f58c4aef8132353ba46c6250a729566f66
sun-jay <sunny.jyrm@gmail.com>
2023-07-07 14:47:57 -0700

    setup

--- src/components/ThemuImg.jsx
+++ src/components/ThemuImg.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { images } from "../constants";
 
 
-const Themu = ({src, alt, className, style_json, desc, test}) => {
+const Themu = ({src, alt, className, style_json_string, desc, test}) => {
 
-    // console.log(test)
+  console.log(style_json_string)
 
-  return <img src={src} alt={alt} className={className} style ={JSON.parse(style_json)}/>;
+  style_json_string? style_json_string = style_json_string : style_json_string = '{}';
+
+  return <img src={src} alt={alt} className={className} style ={JSON.parse(style_json_string)}/>;
 };
 
 export default Themu;
