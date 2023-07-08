1145d46f5103c45a00d569b94e8996f5087f24a7
sun-jay <sunny.jyrm@gmail.com>
2023-07-08 01:30:01 -0700

    setup

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -37,7 +37,7 @@ const AboutUs = () => {
 
       <div className="app__aboutus-content_knife flex__center">
       <ThemuImg
-          style_json='{"width": "auto", "height": "300px"}'
+          style_json_string='{"width": "auto", "height": "300px"}'
           src={'assets/themu_knife.png'}
           alt="about_knife"
         />
